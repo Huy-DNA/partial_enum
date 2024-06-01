fda2ea2978e71dbc1aa6cd79ae50b6ebf5464bce
Huy-DNA <huydo862003@gmail.com>
2024-06-01 19:29:11 +0700

    chore: restructure crate

--- macro/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn partial(input: TokenStream, annotated_item: TokenStream) -> TokenStream {
-    annotated_item
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
-pub mod flags;
-pub use flags::*;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, ItemEnum};
+
+#[proc_macro_attribute]
+pub fn partial(args: TokenStream, annotated_item: TokenStream) -> TokenStream {
+    let parsed_enum = parse_macro_input!(annotated_item as ItemEnum);
+
+    panic!("Not implemented")
+}
